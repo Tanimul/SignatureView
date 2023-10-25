f32db274574e6138d385a2922381592780aebcfa
tanimul <tanimulislam.fuad@gmail.com>
2023-10-25 15:39:34 +0600

    Signature view added.

--- app/src/main/java/bd/com/media365/signatureview/SignatureView.kt
+++ app/src/main/java/bd/com/media365/signatureview/SignatureView.kt
@@ -1,17 +1,25 @@
 package bd.com.media365.signatureview
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 
 class SignatureView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
-    private val path = Path()
+    private val paths =
+        mutableListOf<PathWithMode>() // List to store paths with drawing or erasing mode
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var currentPath: PathWithMode? = null
     private var lastX = 0f
     private var lastY = 0f
+    private var isErasing = false
+    private var isDot = false // Variable to track if it's a dot or a line
 
     init {
         paint.color = Color.BLACK
@@ -20,8 +28,39 @@ class SignatureView(context: Context, attrs: AttributeSet?) : View(context, attr
         paint.strokeWidth = 5f
     }
 
+    fun setLineColor(color: Int) {
+        paint.color = color
+        isErasing = false
+    }
+
+    fun setLineWidth(width: Float) {
+        paint.strokeWidth = width
+        isErasing = false
+    }
+
+    fun updateEraseMode() {
+        isErasing = !isErasing
+    }
+
+    fun undo() {
+        currentPath?.path?.reset() // Clear the current path
+        if (paths.isNotEmpty()) {
+            paths.removeAt(paths.size - 1) // Remove the last path in the list
+        }
+        invalidate()
+    }
+
     override fun onDraw(canvas: Canvas) {
-        canvas.drawPath(path, paint)
+        for (pathWithMode in paths) {
+            paint.color = if (pathWithMode.isErasing) Color.WHITE else Color.BLACK
+            paint.strokeWidth = if (pathWithMode.isErasing) 20f else 5f
+            canvas.drawPath(pathWithMode.path, paint)
+        }
+        currentPath?.let {
+            paint.color = if (isErasing) Color.WHITE else Color.BLACK
+            paint.strokeWidth = if (isErasing) 20f else 5f
+            canvas.drawPath(it.path, paint)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
@@ -30,21 +69,37 @@ class SignatureView(context: Context, attrs: AttributeSet?) : View(context, attr
 
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                path.moveTo(x, y)
+
+                currentPath = if (isErasing) {
+                    val radius = 20f // Replace with your desired radius
+                    PathWithMode(Path(), true, radius)
+                } else {
+                    PathWithMode(Path(), false, 0f)
+                }
+                paths.add(currentPath!!)
+                currentPath?.path?.moveTo(x, y)
                 lastX = x
                 lastY = y
+                isDot = true // Initialize as a dot
                 return true
             }
 
             MotionEvent.ACTION_MOVE -> {
-                val dx = Math.abs(x - lastX)
-                val dy = Math.abs(y - lastY)
-                if (dx >= 5 || dy >= 5) {
-                    path.quadTo(lastX, lastY, (x + lastX) / 2, (y + lastY) / 2)
-                    lastX = x
-                    lastY = y
-                    postInvalidate()
+                currentPath?.path?.lineTo(x, y)
+                lastX = x
+                lastY = y
+                isDot = false // Now it's not a dot, it's a line
+                postInvalidate()
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                if (isDot && !isErasing) {
+                    // Check if it's intended to be a dot (click)
+                    currentPath?.path?.addCircle(x, y, 2.5f, Path.Direction.CW)
                 }
+                postInvalidate()
+                isDot = false // Reset the flag
                 return true
             }
 
@@ -52,15 +107,19 @@ class SignatureView(context: Context, attrs: AttributeSet?) : View(context, attr
         }
     }
 
-    fun clear() {
-        path.reset()
-        invalidate()
-    }
-
     fun getSignatureBitmap(): Bitmap {
         val signatureBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         val canvas = Canvas(signatureBitmap)
-        draw(canvas)
+        for (pathWithMode in paths) {
+            paint.color = if (pathWithMode.isErasing) Color.WHITE else Color.BLACK
+            canvas.drawPath(pathWithMode.path, paint)
+        }
+        currentPath?.let {
+            paint.color = if (isErasing) Color.WHITE else Color.BLACK
+            canvas.drawPath(it.path, paint)
+        }
         return signatureBitmap
     }
+
+
 }
